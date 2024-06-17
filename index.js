14149d1cd1b0d6addec766b3214600bebe830f47
Jeffrey Hung <17494876+Jeffreyhung@users.noreply.github.com>
2024-06-17 13:07:42 -0700

    formatting

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const config = {
-no_ref: "on", //Control the HTTP referrer header, if you want to create an anonymous link that will hide the HTTP Referer header, please set to "on" .
-theme:"theme/urlcool",//Homepage theme, use the empty value for default theme. To use urlcool theme, please fill with "theme/urlcool" .
-cors: "on",//Allow Cross-origin resource sharing for API requests.
-unique_link:true,//If it is true, the same long url will be shorten into the same short url
-custom_link:true,//Allow users to customize the short url.
-safe_browsing_api_key: ${env.safe_browsing_api_key} //Enter Google Safe Browsing API Key to enable url safety check before redirect.
+  no_ref: "on", //Control the HTTP referrer header, if you want to create an anonymous link that will hide the HTTP Referer header, please set to "on" .
+  theme: "theme/urlcool",//Homepage theme, use the empty value for default theme. To use urlcool theme, please fill with "theme/urlcool" .
+  cors: "on",//Allow Cross-origin resource sharing for API requests.
+  unique_link: true,//If it is true, the same long url will be shorten into the same short url
+  custom_link: true,//Allow users to customize the short url.
+  safe_browsing_api_key: ${ env.safe_browsing_api_key } //Enter Google Safe Browsing API Key to enable url safety check before redirect.
 }
 
 const html404 = `<!DOCTYPE html>
@@ -13,15 +13,15 @@ const html404 = `<!DOCTYPE html>
   <p>The url you visit is not found.</p>
 </body>`
 
-let response_header={
+let response_header = {
   "content-type": "text/html;charset=UTF-8",
-} 
+}
 
-if (config.cors=="on"){
-  response_header={
-  "content-type": "text/html;charset=UTF-8",
-  "Access-Control-Allow-Origin":"*",
-  "Access-Control-Allow-Methods": "POST",
+if (config.cors == "on") {
+  response_header = {
+    "content-type": "text/html;charset=UTF-8",
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "POST",
   }
 }
 
@@ -36,54 +36,54 @@ async function randomString(len) {
   return result;
 }
 
-async function sha512(url){
-    url = new TextEncoder().encode(url)
+async function sha512(url) {
+  url = new TextEncoder().encode(url)
 
-    const url_digest = await crypto.subtle.digest(
-      {
-        name: "SHA-512",
-      },
-      url, // The data you want to hash as an ArrayBuffer
-    )
-    const hashArray = Array.from(new Uint8Array(url_digest)); // convert buffer to byte array
-    const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
-    //console.log(hashHex)
-    return hashHex
+  const url_digest = await crypto.subtle.digest(
+    {
+      name: "SHA-512",
+    },
+    url, // The data you want to hash as an ArrayBuffer
+  )
+  const hashArray = Array.from(new Uint8Array(url_digest)); // convert buffer to byte array
+  const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
+  //console.log(hashHex)
+  return hashHex
 }
-async function checkURL(URL){
-    let str=URL;
-    let Expression=/http(s)?:\/\/([\w-]+\.)+[\w-]+(\/[\w- .\/?%&=]*)?/;
-    let objExp=new RegExp(Expression);
-    if(objExp.test(str)==true){
-      if (str[0] == 'h')
-        return true;
-      else
-        return false;
-    }else{
-        return false;
-    }
-} 
-async function save_url(URL){
-    let random_key=await randomString()
-    let is_exist=await LINKS.get(random_key)
-    console.log(is_exist)
-    if (is_exist == null)
-        return await LINKS.put(random_key, URL),random_key
+async function checkURL(URL) {
+  let str = URL;
+  let Expression = /http(s)?:\/\/([\w-]+\.)+[\w-]+(\/[\w- .\/?%&=]*)?/;
+  let objExp = new RegExp(Expression);
+  if (objExp.test(str) == true) {
+    if (str[0] == 'h')
+      return true;
     else
-        save_url(URL)
+      return false;
+  } else {
+    return false;
+  }
 }
-async function is_url_exist(url_sha512){
+async function save_url(URL) {
+  let random_key = await randomString()
+  let is_exist = await LINKS.get(random_key)
+  console.log(is_exist)
+  if (is_exist == null)
+    return await LINKS.put(random_key, URL), random_key
+  else
+    save_url(URL)
+}
+async function is_url_exist(url_sha512) {
   let is_exist = await LINKS.get(url_sha512)
   console.log(is_exist)
   if (is_exist == null) {
     return false
-  }else{
+  } else {
     return is_exist
   }
 }
-async function is_url_safe(url){
+async function is_url_safe(url) {
 
-  let raw = JSON.stringify({"client":{"clientId":"Url-Shorten-Worker","clientVersion":"1.0.7"},"threatInfo":{"threatTypes":["MALWARE","SOCIAL_ENGINEERING","POTENTIALLY_HARMFUL_APPLICATION","UNWANTED_SOFTWARE"],"platformTypes":["ANY_PLATFORM"],"threatEntryTypes":["URL"],"threatEntries":[{"url":url}]}});
+  let raw = JSON.stringify({ "client": { "clientId": "Url-Shorten-Worker", "clientVersion": "1.0.7" }, "threatInfo": { "threatTypes": ["MALWARE", "SOCIAL_ENGINEERING", "POTENTIALLY_HARMFUL_APPLICATION", "UNWANTED_SOFTWARE"], "platformTypes": ["ANY_PLATFORM"], "threatEntryTypes": ["URL"], "threatEntries": [{ "url": url }] } });
 
   let requestOptions = {
     method: 'POST',
@@ -91,50 +91,52 @@ async function is_url_safe(url){
     redirect: 'follow'
   };
 
-  result = await fetch("https://safebrowsing.googleapis.com/v4/threatMatches:find?key="+config.safe_browsing_api_key, requestOptions)
+  result = await fetch("https://safebrowsing.googleapis.com/v4/threatMatches:find?key=" + config.safe_browsing_api_key, requestOptions)
   result = await result.json()
   console.log(result)
-  if (Object.keys(result).length === 0){
+  if (Object.keys(result).length === 0) {
     return true
-  }else{
+  } else {
     return false
   }
 }
 async function handleRequest(request) {
   console.log(request)
   if (request.method === "POST") {
-    let req=await request.json()
+    let req = await request.json()
     console.log(req["url"])
-    if(!await checkURL(req["url"])){
-    return new Response(`{"status":500,"key":": Error: Url illegal."}`, {
-      headers: response_header,
-    })}
-    let stat,random_key
-    if (config.unique_link){
+    if (!await checkURL(req["url"])) {
+      return new Response(`{"status":500,"key":": Error: Url illegal."}`, {
+        headers: response_header,
+      })
+    }
+    let stat, random_key
+    if (config.unique_link) {
       let url_sha512 = await sha512(req["url"])
       let url_key = await is_url_exist(url_sha512)
-      if(url_key){
+      if (url_key) {
         random_key = url_key
-      }else{
-        stat,random_key=await save_url(req["url"])
-        if (typeof(stat) == "undefined"){
-          console.log(await LINKS.put(url_sha512,random_key))
+      } else {
+        stat, random_key = await save_url(req["url"])
+        if (typeof (stat) == "undefined") {
+          console.log(await LINKS.put(url_sha512, random_key))
         }
       }
-    }else{
-      stat,random_key=await save_url(req["url"])
+    } else {
+      stat, random_key = await save_url(req["url"])
     }
     console.log(stat)
-    if (typeof(stat) == "undefined"){
-      return new Response(`{"status":200,"key":"/`+random_key+`"}`, {
-      headers: response_header,
-    })
-    }else{
+    if (typeof (stat) == "undefined") {
+      return new Response(`{"status":200,"key":"/` + random_key + `"}`, {
+        headers: response_header,
+      })
+    } else {
       return new Response(`{"status":200,"key":": Error:Reach the KV write limitation."}`, {
-      headers: response_header,
-    })}
-  }else if(request.method === "OPTIONS"){  
-      return new Response(``, {
+        headers: response_header,
+      })
+    }
+  } else if (request.method === "OPTIONS") {
+    return new Response(``, {
       headers: response_header,
     })
 
@@ -145,33 +147,33 @@ async function handleRequest(request) {
   const params = requestURL.search;
 
   console.log(path)
-  if(!path){
+  if (!path) {
 
-    const html= await fetch("http://jeffreyhung.com/tyrion/"+config.theme+"/index.html")
-    
+    const html = await fetch("http://jeffreyhung.com/tyrion/" + config.theme + "/index.html")
+
     return new Response(await html.text(), {
-    headers: {
-      "content-type": "text/html;charset=UTF-8",
-    },
-  })
+      headers: {
+        "content-type": "text/html;charset=UTF-8",
+      },
+    })
   }
 
   const value = await LINKS.get(path);
-  let location ;
+  let location;
 
-  if(params) {
+  if (params) {
     location = value + params
   } else {
-      location = value
+    location = value
   }
   console.log(value)
-  
+
 
   if (location) {
-    if (config.safe_browsing_api_key){
-      if(!(await is_url_safe(location))){
+    if (config.safe_browsing_api_key) {
+      if (!(await is_url_safe(location))) {
         let warning_page = await fetch("http://jeffreyhung.com/tyrion/safe-browsing.html")
-        warning_page =await warning_page.text()
+        warning_page = await warning_page.text()
         warning_page = warning_page.replace(/{Replace}/gm, location)
         return new Response(warning_page, {
           headers: {
@@ -180,19 +182,19 @@ async function handleRequest(request) {
         })
       }
     }
-    if (config.no_ref=="on"){
-      let no_ref= await fetch("http://jeffreyhung.com/tyrion/no-ref.html")
-      no_ref=await no_ref.text()
-      no_ref=no_ref.replace(/{Replace}/gm, location)
+    if (config.no_ref == "on") {
+      let no_ref = await fetch("http://jeffreyhung.com/tyrion/no-ref.html")
+      no_ref = await no_ref.text()
+      no_ref = no_ref.replace(/{Replace}/gm, location)
       return new Response(no_ref, {
-      headers: {
-        "content-type": "text/html;charset=UTF-8",
-      },
-    })
-    }else{
+        headers: {
+          "content-type": "text/html;charset=UTF-8",
+        },
+      })
+    } else {
       return Response.redirect(location, 302)
     }
-    
+
   }
   // If request not in kv, return 404
   return new Response(html404, {
